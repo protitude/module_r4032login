d312d9e425362dea5fd1fc5041fa0aac3b763667
bdone <brentd@knowclassic.com>
2014-02-28 12:28:22 -0500

    Issue #2189179 by bdone: Remove replacement of AccessSubscriber and only subscribe to EXECPTION

--- lib/Drupal/r4032login/EventSubscriber/R4032LoginSubscriber.php
+++ lib/Drupal/r4032login/EventSubscriber/R4032LoginSubscriber.php
@@ -7,11 +7,12 @@
 
 namespace Drupal\r4032login\EventSubscriber;
 
-use Drupal\Core\KeyValueStore\StateInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\DestructableInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Redirect 403 to User Login event subscriber.
@@ -19,33 +20,58 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class R4032LoginSubscriber implements EventSubscriberInterface, DestructableInterface {
 
   /**
-   * The state keyvalue collection.
+   * The current user.
    *
-   * @var \Drupal\Core\KeyValueStore\StateInterface
+   * @var \Drupal\Core\Session\AccountInterface
    */
-  protected $state;
+  protected $currentUser;
 
   /**
-   * Constructor.
+   * Constructs a new R4032LoginSubscriber.
    *
-   * @param \Drupal\Core\KeyValueStore\StateInterface $state
-   *   The state key value store.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
    */
-  public function __construct(StateInterface $state) {
-    $this->state = $state;
+  public function __construct(AccountInterface $current_user) {
+    $this->currentUser = $current_user;
   }
 
   /**
-   * Verifies that the current user can access the requested path.
+   * Redirects on 403 Access Denied kernel exceptions.
    *
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The Event to process.
    *
-   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
-   *   Thrown when the access got denied.
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   A response that redirects 403 Access Denied pages user login page.
    */
-  public function onKernelRequestAccessCheck(GetResponseEvent $event) {
-    drupal_set_message(t('R4032LoginSubscriber event'));
+  public function onKernelExceptionCheck(GetResponseEvent $event) {
+    $config = \Drupal::config('r4032login.settings');
+    if ($this->currentUser->isAnonymous()) {
+      // Show custom access denied message if set.
+      if ($config->get('display_denied_message')) {
+        $message = $config->get('access_denied_message');
+        drupal_set_message($message, 'error');
+      }
+      // Handle redirection to the login form.
+      $login_path = $config->get('user_login_path');
+      $code = $config->get('default_redirect_code');
+      $response = new RedirectResponse(url($login_path, array('absolute' => TRUE, 'query' => drupal_get_destination())), $code);
+      $event->setResponse($response);
+    }
+    else {
+      // Check to see if we are to redirect the user.
+      $redirect = $config->get('redirect_authenticated_users_to');
+      if ($redirect) {
+        // Custom access denied page for logged in users.
+        $response = new RedirectResponse(url($redirect, array('absolute' => TRUE)));
+        $event->setResponse($response);
+      }
+      else {
+        // Display the default access denied page.
+        throw new AccessDeniedHttpException();
+      }
+    }
   }
 
   /**
@@ -55,7 +81,7 @@ class R4032LoginSubscriber implements EventSubscriberInterface, DestructableInte
    *   An array of event listener definitions.
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::EXCEPTION][] = array('onKernelRequestAccessCheck', 0);
+    $events[KernelEvents::EXCEPTION][] = array('onKernelExceptionCheck', 0);
     return $events;
   }
 
